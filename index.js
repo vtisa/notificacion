ad89096f7dc0203579f8ff61ce33b21a697b47c3
vtisa <isai.valqui@tecsup.edu.pe>
2025-02-05 10:08:58 -0500

    first

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-require('dotenv').config(); // Importa dotenv
+// Servidor Node.js actualizado para Bingo
+require('dotenv').config();
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
@@ -12,7 +13,7 @@ const CORS_ORIGIN = process.env.CORS_ORIGIN || '*';
 const io = new Server(server, {
   cors: {
     origin: CORS_ORIGIN,
-  }
+  },
 });
 
 let rooms = {};
@@ -20,18 +21,26 @@ let rooms = {};
 io.on('connection', (socket) => {
   console.log('Nuevo cliente conectado:', socket.id);
 
-  socket.on('createRoom', (callback) => {
+  socket.on('createRoom', ({ coordinatorName, winningRules, prizes }, callback) => {
     const roomCode = Math.random().toString(36).substring(2, 8).toUpperCase();
-    rooms[roomCode] = { coordinator: socket.id, players: [], numbers: [] };
+    rooms[roomCode] = {
+      coordinator: socket.id,
+      coordinatorName,
+      players: [],
+      numbers: [],
+      winningRules,
+      prizes,
+      currentPrizeIndex: 0
+    };
     socket.join(roomCode);
-    callback(roomCode);
+    callback({ roomCode, coordinatorName });
   });
 
-  socket.on('joinRoom', (roomCode, callback) => {
+  socket.on('joinRoom', ({ roomCode, playerName }, callback) => {
     if (rooms[roomCode]) {
       socket.join(roomCode);
-      rooms[roomCode].players.push(socket.id);
-      callback({ success: true });
+      rooms[roomCode].players.push({ id: socket.id, name: playerName });
+      callback({ success: true, drawnNumbers: rooms[roomCode].numbers, winningRules: rooms[roomCode].winningRules });
     } else {
       callback({ success: false, message: 'Sala no encontrada' });
     }
@@ -44,13 +53,25 @@ io.on('connection', (socket) => {
     }
   });
 
-  socket.on('declareWinner', (roomCode, winnerName) => {
-    io.to(roomCode).emit('winnerDeclared', winnerName);
+  socket.on('declareWinner', ({ roomCode, winnerName }) => {
+    if (rooms[roomCode]) {
+      io.to(roomCode).emit('winnerDeclared', winnerName);
+      const currentPrize = rooms[roomCode].prizes[rooms[roomCode].currentPrizeIndex];
+      io.to(roomCode).emit('selectingPrize', { winnerName, currentPrize });
+    }
   });
 
-  socket.on('endGame', (roomCode) => {
-    io.to(roomCode).emit('gameEnded');
-    delete rooms[roomCode];
+  socket.on('assignPrize', ({ roomCode, prize }) => {
+    io.to(roomCode).emit('prizeAssigned', prize);
+    rooms[roomCode].currentPrizeIndex++;
+
+    if (rooms[roomCode].currentPrizeIndex < rooms[roomCode].prizes.length) {
+      io.to(roomCode).emit('nextRound', 'Iniciando siguiente ronda...');
+      rooms[roomCode].numbers = [];
+    } else {
+      io.to(roomCode).emit('gameEnded', 'Juego terminado');
+      delete rooms[roomCode];
+    }
   });
 
   socket.on('disconnect', () => {
@@ -58,10 +79,10 @@ io.on('connection', (socket) => {
     for (const roomCode in rooms) {
       const room = rooms[roomCode];
       if (room.coordinator === socket.id) {
-        io.to(roomCode).emit('gameEnded');
+        io.to(roomCode).emit('gameEnded', 'El coordinador se ha desconectado.');
         delete rooms[roomCode];
       } else {
-        room.players = room.players.filter((id) => id !== socket.id);
+        room.players = room.players.filter((player) => player.id !== socket.id);
       }
     }
   });
